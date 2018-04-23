80d90dd82ba0e2185967c0119a23686dccd2d313
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/E1517/DXGrid_ShowCheckBoxInColumnHeaders/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿// Developer Express Code Central Example:
+// How to display a check box within column headers
+// 
+// This example demonstrates how to display a check box within column headers. If
+// the check box is checked, the corresponding column is made read-only. Uncheck
+// the check box to allow column values to be edited.
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E1517
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace DXGrid_ShowCheckBoxInColumnHeaders {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
