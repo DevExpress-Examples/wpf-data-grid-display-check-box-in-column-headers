f9f334e79720675cf99ffb3dc22b51e1bf15178c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGrid_ShowCheckBoxInColumnHeaders/Window1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Editors;
+using DevExpress.Xpf.Grid;
+
+namespace DXGrid_ShowCheckBoxInColumnHeaders {
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+            grid.DataSource = new nwindDataSetTableAdapters.ProductsTableAdapter().GetData();
+        }
+
+    }
+}
---
+++ VB/DXGrid_ShowCheckBoxInColumnHeaders/Window1.xaml.vb
@@ -0,0 +1,27 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.Editors
+Imports DevExpress.Xpf.Grid
+
+Namespace DXGrid_ShowCheckBoxInColumnHeaders
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			grid.DataSource = New nwindDataSetTableAdapters.ProductsTableAdapter().GetData()
+		End Sub
+
+    End Class
+End Namespace
